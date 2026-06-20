f31f99208a30218108a0a3abe8d326eff358287c
vanngule31-star <vanngule31@gmail.com>
2026-06-20 21:24:55 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run(TOKEN)
