313dae5345e9777c58dc38a72a6842673a4b1784
vanngule31-star <vanngule31@gmail.com>
2026-06-20 22:21:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,30 +4,15 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True  # cần để đọc tin nhắn !test
+intents.message_content = True  # bật nếu cần đọc chat
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    # test bot
-    if message.content == "!test":
-        await message.channel.send("bot cua toi day")
-
-    # (chuẩn bị sẵn) lệnh vào voice sau này
-    if message.content.startswith("!join"):
-        if message.author.voice:
-            channel = message.author.voice.channel
-            await channel.connect()
-            await message.channel.send("Da vao voice!")
-        else:
-            await message.channel.send("Ban khong o trong voice!")
+    print(f"Bot đã online: {client.user}")
+    await client.change_presence(
+        activity=discord.Game(name="AFK 24/7")
+    )
 
 client.run(TOKEN)
