011b30c5f0aa1bb0ae9fa6b02d0cd1fdfbec0562
vanngule31-star <vanngule31@gmail.com>
2026-06-20 22:51:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,15 +4,13 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True  # bật nếu cần đọc chat
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f"Bot đã online: {client.user}")
-    await client.change_presence(
-        activity=discord.Game(name="AFK 24/7")
-    )
+    await client.change_presence(activity=discord.Game(name="AFK 24/7"))
 
 client.run(TOKEN)
